96dbd94e4fde2fa872ec3032820d7d7f5e0e8305
韦祖策 <mf1732135@smail.nju.edu.cn>
2017-11-18 00:24:15 +0800

    修正debug部分

--- src/cn/nju/edu/software/CleanLogic/DebugInfoCleanUtil.java
+++ src/cn/nju/edu/software/CleanLogic/DebugInfoCleanUtil.java
@@ -16,12 +16,17 @@ import java.util.List;
  */
 public class DebugInfoCleanUtil {
 
-    public void cleanDebufInfo(int sid,int eid,String projectName){
-        DebugModel model=new DebugModel();
-        model.setProjectName(projectName);
-        model.setSid(sid);
-        model.setBreakPointNum(breakPointNum(projectName));
-        model.setDebugRunNum(debugRunNum(projectName));
+    public void cleanDebufInfo(int sid,int eid){
+        List<String> projectNames=projetcName();
+        List<DebugModel> list= new ArrayList<>();
+        for(String projectName:projectNames){
+            DebugModel model=new DebugModel();
+            model.setProjectName(projectName);
+            model.setSid(sid);
+            model.setBreakPointNum(breakPointNum(projectName));
+            model.setDebugRunNum(debugRunNum(projectName));
+            list.add(model);
+        }
     }
 
     private  int breakPointNum(String projectName){
@@ -96,11 +101,11 @@ public class DebugInfoCleanUtil {
         Connection connection= DaoUtil.getMySqlConnection(ConstantConfig.TEMPBASE);
         ResultSet set=null;
         try {
-            PreparedStatement prepar=connection.prepareStatement("select DISTINCT debug_targetfrom debug_info");
+            PreparedStatement prepar=connection.prepareStatement("select DISTINCT debug_target from debug_info");
             //把sql语句发送到数据库，得到预编译类的对象，这句话是选择该student表里的所有数据
             set=prepar.executeQuery();
             while(set.next()) {
-                
+                list.add(set.getString("debug_target"));
             }
         } catch (SQLException e) {
             e.printStackTrace();
